ecfeb133315d8566444df962a24e92ffe996f1cb
JohnBasco-prog <jnrbasco@brc.pshs.edu.ph>
2026-09-17 11:34:31 +0800

    Add Chinese Zodiac sign printing logic

---
+++ q1/Chinese_Zodiac.py
@@ -0,0 +1,17 @@
+    print("Your Chinese Zodiac Sign is: Rabbit (兔 / Tù)")
+elif zodiac == 4:
+    print("Your Chinese Zodiac Sign is: Dragon (龙 / Lóng)")
+elif zodiac == 5:
+    print("Your Chinese Zodiac Sign is: Snake (蛇 / Shé)")
+elif zodiac == 6:
+    print("Your Chinese Zodiac Sign is: Horse (马 / Mǎ)")
+elif zodiac == 7:
+    print("Your Chinese Zodiac Sign is: Goat (羊 / Yáng)")
+elif zodiac == 8:
+    print("Your Chinese Zodiac Sign is: Monkey (猴 / Hóu)")
+elif zodiac == 9:
+    print("Your Chinese Zodiac Sign is: Rooster (鸡 / Jī)")
+elif zodiac == 10:
+    print("Your Chinese Zodiac Sign is: Dog (狗 / Gǒu)")
+else:
+    print("Your Chinese Zodiac Sign is: Pig (猪 / Zhū)")
